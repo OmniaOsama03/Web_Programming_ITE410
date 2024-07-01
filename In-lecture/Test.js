156e86cea3a61f32b03a8d507b4530919e0665c6
OmniaOsama03 <Omniaosama432@gmail.com>
2024-07-01 21:15:11 +0400

    Test

---
+++ In-lecture/Test.js
@@ -0,0 +1,24 @@
+let user = null;
+
+console.log(user);
+console.log(typeof user);
+
+console.log("Try to run this in Node js");
+
+let x = 5;
+console.log(x);
+console.log(typeof x);
+
+x = "Omnia"; //overwriting
+console.log(x);
+console.log(typeof x);
+
+let number = 3;
+console.log(typeof (number/"hi"));
+
+console.log("----------------------------------------");
+
+let bigNumber = 400n; //notice the 'n' at the end
+console.log(bigNumber);
+console.log("type: " + typeof bigNumber);
+
